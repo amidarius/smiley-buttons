467e2105e0b14ba84e8a98febdcaf90bc58d137d
amidarius <amidarius@yahoo.com>
2021-11-19 12:12:57 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 input.onButtonPressed(Button.A, function () {
+    stop = 1
     basic.showLeds(`
         # # . # #
         # # . # #
@@ -6,43 +7,51 @@ input.onButtonPressed(Button.A, function () {
         # . . . #
         . # # # .
         `)
-    stop = 1
 })
 input.onButtonPressed(Button.AB, function () {
     stop = 0
     while (stop == 0) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # . # .
-            . # # # .
-            . . . . .
-            `)
-        basic.showLeds(`
-            # # # # #
-            # . . . #
-            # . . . #
-            # . . . #
-            # # # # #
-            `)
+        if (stop == 0) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+        }
+        if (stop == 0) {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # . # .
+                . # # # .
+                . . . . .
+                `)
+        }
+        if (stop == 0) {
+            basic.showLeds(`
+                # # # # #
+                # . . . #
+                # . . . #
+                # . . . #
+                # # # # #
+                `)
+        }
+        if (stop == 0) {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # . # .
+                . # # # .
+                . . . . .
+                `)
+        }
     }
     stop = 0
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
 })
 input.onButtonPressed(Button.B, function () {
+    stop = 1
     basic.showLeds(`
         # # . # #
         # # . # #
@@ -50,10 +59,70 @@ input.onButtonPressed(Button.B, function () {
         . # # # .
         # . . . #
         `)
-    stop = 1
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    stop = 0
+    while (stop == 0) {
+        if (stop == 0) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+        }
+        if (stop == 0) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . # . .
+                . # . # .
+                . . . . .
+                `)
+        }
+        if (stop == 0) {
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . . . .
+                . # . # .
+                # . . . #
+                `)
+        }
+        if (stop == 0) {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . #
+                `)
+        }
+        if (stop == 0) {
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . . . .
+                . # . # .
+                # . . . #
+                `)
+        }
+        if (stop == 0) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . # . .
+                . # . # .
+                . . . . .
+                `)
+        }
+    }
+    stop = 0
 })
 let stop = 0
 stop = 0
+led.setBrightness(4)
 basic.forever(function () {
 	
 })
